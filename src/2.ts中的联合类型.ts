074289d7d3056485734a11c012086d8f5ff273a2
cjshinningg <784319417@qq.com>
2024-04-08 11:08:44 +0800

    5.函数的使用

---
+++ src/2.ts中的联合类型.ts
@@ -0,0 +1,30 @@
+// 联合类型，如果不进行初始化操作，必须要给类型，否则都是any
+let numOrStr: string | number;
+// 默认联合类型，在没有确定类型之前，只能调用两个类型共同方法
+// 在变量确定类型后，可以设置对应的方法
+numOrStr = 'abc';
+numOrStr = 123;
+
+// 如果赋予类型后，可以根据上下文自动推断对应类型的方法
+const ele: HTMLElement | null = document.getElementById('app');
+// ele && (ele.innerHTML = 'abc');
+// as / <> 非空断言，表示这个东西一定有值，告诉ts按照我的想法来，如果后续出错自己负责，一定不为空
+// ele!.innerHTML = 'abc';
+
+// 直接强转某个类型，强制告诉人家，这个类型就是里面的某一个
+let a: string | number | undefined;
+(a as any) as boolean;  //双重断言，先转化成any，再转化成一个具体的类型，可能会导致类型出问题
+
+// ? 等价于 aa && aa.xxx  链判断运算符  ？是js中就存在的
+// ele?.style.color
+
+let a1 = false ?? true  //?? 表示排除 null和undefined
+console.log(a1);
+
+// 字面量类型 类型的内容是固定的 枚举
+// 如果类型过于复杂，希望后续复用，可以把类型单独提取出来
+type IType = 'a' | 'b' | 'c' | 'd'; //类型别名
+let type: IType = 'b';
+let type2: IType = 'c';
+
+export { }
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,49 @@
-// 联合类型，如果不进行初始化操作，必须要给类型，否则都是any
-let numOrStr: string | number;
-// 默认联合类型，在没有确定类型之前，只能调用两个类型共同方法
-// 在变量确定类型后，可以设置对应的方法
-numOrStr = 'abc';
-numOrStr = 123;
+// 函数 对函数增加类型 对函数的返回值进行类型校验 对函数的本身来校验
 
-// 如果赋予类型后，可以根据上下文自动推断对应类型的方法
-const ele: HTMLElement | null = document.getElementById('app');
-// ele && (ele.innerHTML = 'abc');
-// as / <> 非空断言，表示这个东西一定有值，告诉ts按照我的想法来，如果后续出错自己负责，一定不为空
-// ele!.innerHTML = 'abc';
+// 考虑函数的参数和返回值 function关键字|表达式声明
 
-// 直接强转某个类型，强制告诉人家，这个类型就是里面的某一个
-let a: string | number | undefined;
-(a as any) as boolean;  //双重断言，先转化成any，再转化成一个具体的类型，可能会导致类型出问题
+// function sum(x: string, y: string): string {
+//   return x + y;
+// }
 
-// ? 等价于 aa && aa.xxx  链判断运算符  ？是js中就存在的
-// ele?.style.color
+// 函数关键字，写完后会对当前函数自动推断类型
+function sum1(x: number, y: string): string {
+  return x + y;
+}
 
-false ?? true  //?? 表示排除 null和undefined
+// 1.自动根据当前等号邮编的内容，推断左边的类型
+// 2.我们可以知道类型，赋予一个可以兼容这个类型的函数
+const sum2 = (x: number, y: number) => {
+  return x + y;
+}
+sum2(1, 2);
 
-// 字面量类型 类型的内容是固定的 枚举
-// 如果类型过于复杂，希望后续复用，可以把类型单独提取出来
-type IType = 'a' | 'b' | 'c' | 'd'; //类型别名
-let type: IType = 'b';
-let type2: IType = 'c';
+// ? 表示可传可不传
+// const sum3 = (x: string, y?: string): string => {
+//   return x + y;
+// }
+const sum3 = (x: number, y?: number, ...args: number[]): number => {
+  return x + (y as number);
+}
+
+sum3(1, 2, 3, 4, 5);
+
+// 函数重载
+// 123 => [1, 2, 3]
+// 'abc'=> [1, 2, 3]
+// 一个方法，根据参数的不同实现不同的功能，ts的目的就是根据不同的参数返回类型
+function toArray(value: string): string[]
+function toArray(value: number): number[]
+function toArray(value: number | string): number[] | string[] { //重载方法在真实方法的上面
+  if (typeof value == 'string') {
+    return value.split('');
+  } else {
+    return value.toString().split('').map(item => Number(item));
+  }
+}
+
+let r = toArray(123);
+// ts 为了安全，为了更好的提示
+console.log(r);
 
 export { }
